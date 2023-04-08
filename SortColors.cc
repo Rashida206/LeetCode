d65b9ba4cfec6e5c104729384576cd8d6bf9ea8b
Rashida Kadiwala <rashidakadiwala206@gmail.com>
2023-04-08 15:46:40 +0530

    First commit

---
+++ SortColors.cc
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int counter[3]={0}, color=0;
+        
+        for (int i = 0; i < nums.size(); i++) 
+            counter[nums[i]]++;
+        
+        int idx=0;
+        for(int i=0;i<=2;i++){
+            for(int j=0; j < counter[i]; j++)
+                nums[color++]=i;
+        }
+    }
+};
